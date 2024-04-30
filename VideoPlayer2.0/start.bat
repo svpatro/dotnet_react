aabcab3623a739816b6b6fd4d67a0f2b3b7b882c
Shamik Patro <patroshamik@gmail.com>
2024-04-30 18:05:09 -0500

    adjusted Windows start script

--- VideoPlayer2.0/start.bat
+++ VideoPlayer2.0/start.bat
@@ -1,2 +1,2 @@
-start cmd.exe /k "cd /d %~dp0 && dotnet watch"
-start cmd.exe /k "cd /d %~dp0 && npm run dev"
+start cmd.exe /k "cd /d %~dp0 && cd API && dotnet watch"
+start cmd.exe /k "cd /d %~dp0 && cd client-app && npm run dev"
