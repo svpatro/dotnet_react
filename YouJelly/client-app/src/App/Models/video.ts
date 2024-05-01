66aeaeaa7b0a7d0c15ef63418b443783f136296b
Shamik Patro <patroshamik@gmail.com>
2024-05-01 17:49:45 -0500

    Updated Bash Script

---
+++ YouJelly/client-app/src/App/Models/video.ts
@@ -0,0 +1,10 @@
+export type Video = {
+    id: string
+    title: string
+    date: string
+    description: string
+    category: string
+    rating: string
+    visibility: string
+    tags: string
+}
