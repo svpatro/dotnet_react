db43c7cbb88ad9aea9ce9d4738455d6ef87ba354
Shamik Patro <patroshamik@gmail.com>
2024-05-01 15:15:02 -0500

    Kestrel server integration

--- VideoPlayer2.0/client-app/src/App/Layout/App.tsx
+++ VideoPlayer2.0/client-app/src/App/Layout/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import './App.css'
 import axios from 'axios';
 
 function App() {
---
+++ VideoPlayer2.0/client-app/src/App/Models/video.ts
@@ -0,0 +1,10 @@
+export interface Root {
+    id: string
+    title: string
+    date: string
+    description: string
+    category: string
+    rating: string
+    visibility: string
+    tags: string
+}
